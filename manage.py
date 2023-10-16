d4ac1c494bde0f83750f613a40bb819f292a8073
Julian grijuela <juliangc24@educastur.es>
2023-10-16 09:26:58 +0200

    Error critico solucionado

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+#estoy corriguinedo un error
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locallibrary.settings')
